49eb00d48b1850d9a960ef208c94debb1c491632
Meet Bhagdev <meetbhagdev@gmail.com>
2022-02-25 17:10:10 -0800

    updating JSON

--- view/sections/abilities.inc.php
+++ view/sections/abilities.inc.php
@@ -53,10 +53,7 @@
 	
 </div>
 
-<div class="text-center project-referal">
-	<p>This project is build on a custom made PHP framework.</p>
-	<a href="https://github.com/pascalvgemert/resume" class="btn btn-primary" target="_blank">See project on Github</a>
-</div>
+
 
 <hr />
 
@@ -110,48 +107,5 @@
 
 <hr />
 
-<h3>Tools</h3>
 
-<div class="row">
-	<?php if(count($tools) > 0) { ?>
-	
-	<div class="col-md-6">
-		<ul class="no-bullets">
-		
-		<?php foreach($tools as $index => $tool) { ?>
-		
-			<li>
-				<span class="ability-title"><?= $tool->title; ?> (<?= $tool->endorsement; ?>)</span>
-				<span class="ability-score">
-				
-				<?php for($stars = 1; $stars <= 5; $stars++) { ?>
-				
-					<span class="glyphicon glyphicon-star <?= ($tool->level >= $stars) ? 'filled' : ''; ?>"></span>
-					
-				<?php } ?>
-					
-				</span>
-			</li>
-			
-			<?php if(ceil(count($tools) / 2) == $index + 1) { ?> 
-			
-		</ul>
-	</div>
-	<div class="col-md-6">
-		<ul class="no-bullets">
-			
-			<?php } ?>
-		
-		<?php } ?>
-		
-		</ul>
-	</div>
-	
-	<?php } else { ?>
-	
-	<div class="alert alert-warning">
-		No languages were found in this resume
-	</div>
-	
-	<?php } ?>
 </div>
